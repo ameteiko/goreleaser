70895228027c3e8695e6527f76271902418f504a
Oleksandr Meteiko <oleksandr.meteiko@sap.com>
2023-08-22 15:06:19 +0200

    Adds a new line

--- .commitlint.js
+++ .commitlint.js
@@ -1,10 +1,10 @@
 module.exports = {
     rules: {
         "scope-case": [2, "always", "lower-case"],
-        "subject-case": [2, "always", "lower-case"],
+        "subject-case": [2, "always", ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
         "subject-empty": [2, "never"],
         "subject-full-stop": [2, "never", "."],
-        "subject-max-length": [2, "always", "40"],
+        "subject-max-length": [2, "always", "120"],
         "subject-min-length": [2, "always", "5"],
         "type-enum": [2, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert']],
         "type-case": [2, "always", "lower-case"],
