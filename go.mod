c94e040377e4030b4679842fa96b48f523471bc1
Oleksandr Meteiko <oleksandr.meteiko@sap.com>
2023-11-30 01:16:37 +0100

    commit

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ameteiko/goreleaser
 go 1.20
 
 require (
+	github.com/ameteiko/errors v0.9.1
 	github.com/google/uuid v1.3.0
 	gopkg.in/yaml.v3 v3.0.1
 )
--- internal/iotl/render.go
+++ internal/iotl/render.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/google/uuid"
+	"github.com/ameteiko/errors"
 	"gopkg.in/yaml.v3"
 )
 
@@ -11,6 +12,7 @@ func Render() {
 	fmt.Println(uuid.New().String())
 	fmt.Println(yaml.ScalarNode)
 
+	_ = errors.New("test")
 }
 
 func a() {}
